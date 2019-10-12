25fc7efebfdfc03817b95e85baaa61707faa5116
Glenlivet Zhang <glenlivet.zhang@pwc.com>
2019-10-12 09:51:19 +0800

    setup a responsive image container.

--- src/web/frontend/main.tsx
+++ src/web/frontend/main.tsx
@@ -7,9 +7,12 @@ const App = () => {
     const [c, increment] = useState(0);
 
     return (
-        <div className="App">
+        <div className="app">
             <h1>{c}</h1>
             <button onClick={() => increment(c + 1)}>Press me</button>
+            <div className="img-container">
+              <img src="https://images.dog.ceo/breeds/terrier-norfolk/n02094114_1972.jpg"/>
+            </div>
         </div>
     )
 };
